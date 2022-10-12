0a06cc3a0fa0e137c6dce60cce644fe2d8e57fd3
Guillermo Verón <guiver95@gmail.com>
2022-10-12 20:23:29 -0300

    adding event listener to keyDown to toggle read and archive

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="email-display">
+    <div>
+      <button @click="toggleArchive">{{email.archived ? 'Move to inbox (e)' : 'Archive (e)'}}</button>
+      <button @click="toggleRead">{{ email.read ? "Mark unread (r)" : "Mark read (r)" }}</button>
+      <button>Newer</button>
+      <button>Older</button>
+    </div>
     <h2 class="mb-0">
       Subject: <strong>{{ email.subject }}</strong>
     </h2>
@@ -13,6 +19,9 @@
 <script>
 import format from "date-fns/format";
 import marked from "marked";
+import axios from "axios";
+import useKeyDown from "@/composables/useKeyDown";
+
 export default {
   props: {
     email: {
@@ -21,11 +30,27 @@ export default {
     }
   },
   setup(props) {
+    useKeyDown("r", () => toggleRead());
+    useKeyDown("e", () => toggleArchive());
+
+    let toggleRead = () => {
+      props.email.read = !props.email.read;
+      axios.put(`http://localhost:3000/emails/${props.email.id}`, props.email);
+    };
+
+    let toggleArchive = () => {
+      props.email.archived = !props.email.archived;
+      axios.put(`http://localhost:3000/emails/${props.email.id}`, props.email);
+    };
+
     return {
       format,
-      marked
+      marked,
+      toggleRead,
+      toggleArchive
     };
-  }
+  },
+  methods: {}
 };
 </script>
 
